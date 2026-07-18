57f9ae299585d84b202dcc09779b59130de1c2be
DonellaB <donellaboyd@sandiego.edu>
2026-07-17 22:13:34 -0500

    Prepare first action for release with tagging
    Add files to git and commit with a tag for the first action release.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,7 @@ echo "time=$time" >> $GITHUB_OUTPUT
 
 git add entrypoint.sh
 git update-index --chmod=+x entrypoint.sh
+git add action.yml entrypoint.sh Dockerfile README.md
+git commit -m "My first action is ready"
+git tag -a -m "My first action release" v1
+git push --follow-tags
