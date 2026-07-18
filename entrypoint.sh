1da30b331314cc89e1c8ffa80bdb12387c6b3247
DonellaB <donellaboyd@sandiego.edu>
2026-07-17 23:41:39 -0500

    Remove git commands from entrypoint.sh
    Removed git commands for adding, committing, and pushing changes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,3 @@ echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
-git add entrypoint.sh
-git update-index --chmod=+x entrypoint.sh
-git add action.yml entrypoint.sh Dockerfile README.md
-git commit -m "My first action is ready"
-git tag -a -m "My first action release" v1
-git push --follow-tags
